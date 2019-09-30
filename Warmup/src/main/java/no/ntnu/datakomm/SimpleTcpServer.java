52e47e7eeb8ba90b349db53aa0a5a1ffc4da33d9
nilsjha <nils@gaupne.net>
2019-09-30 18:56:25 +0200

    reformat code style

--- Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
+++ Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
@@ -10,32 +10,32 @@ import java.net.Socket;
  */
 public class SimpleTcpServer
 {
-    private static final int PORT = 1301;
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter output;
-    private BufferedReader input;
-    private boolean keepRunning = true;
-    
-    public static void main(String[] args) {
-        SimpleTcpServer server = new SimpleTcpServer();
-        log("Simple TCP server starting");
-        server.run();
-        log("ERROR: the server should never go out of the run() method! After handling one client");
-    }
-    
-    /**
-     * Log a message to the system console.
-     *
-     * @param message The message to be logged (printed).
-     */
-    private static void log(String message) {
-        System.out.println(message);
-    }
-    
-    public void run() {
-        while (keepRunning) {
+        private static final int PORT = 1301;
+        private ServerSocket serverSocket;
+        private Socket clientSocket;
+        private PrintWriter output;
+        private BufferedReader input;
+        private boolean keepRunning = true;
         
+        public static void main(String[] args) {
+                SimpleTcpServer server = new SimpleTcpServer();
+                log("Simple TCP server starting");
+                server.run();
+                log("ERROR: the server should never go out of the run() method! After handling one client");
         }
-    }
+        
+        /**
+         * Log a message to the system console.
+         *
+         * @param message The message to be logged (printed).
+         */
+        private static void log(String message) {
+                System.out.println(message);
+        }
+        
+        public void run() {
+                while (keepRunning) {
+                
+                }
+        }
 }
