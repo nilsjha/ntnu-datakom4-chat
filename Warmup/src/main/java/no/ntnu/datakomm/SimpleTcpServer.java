2ebf95141ccbc3e1d0601bfead3a32dcc8559adf
nilsjha <nils@gaupne.net>
2019-09-30 18:40:22 +0200

    added constructor with main fields

--- Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
+++ Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
@@ -1,22 +1,29 @@
 package no.ntnu.datakomm;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * A Simple TCP client, used as a warm-up exercise for assignment A4.
  */
-public class SimpleTcpServer {
+public class SimpleTcpServer
+{
+    private static final int PORT = 1301;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter output;
+    private BufferedReader input;
+    private boolean keepRunning = true;
+    
     public static void main(String[] args) {
         SimpleTcpServer server = new SimpleTcpServer();
         log("Simple TCP server starting");
         server.run();
         log("ERROR: the server should never go out of the run() method! After handling one client");
     }
-
-    public void run() {
-        // TODO - implement the logic of the server, according to the protocol.
-        // Take a look at the tutorial to understand the basic blocks: creating a listening socket,
-        // accepting the next client connection, sending and receiving messages and closing the connection
-    }
-
+    
     /**
      * Log a message to the system console.
      *
@@ -24,5 +31,11 @@ public class SimpleTcpServer {
      */
     private static void log(String message) {
         System.out.println(message);
+    }
+    
+    public void run() {
+        while (keepRunning) {
+        
+        }
     }
 }
