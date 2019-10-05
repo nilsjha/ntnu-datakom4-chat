af55a7c46d32da05b33cf3a041fd0ae391c909bf
nilsjha <nils@gaupne.net>
2019-10-06 01:41:51 +0200

    Quality, replaced  wildcard class import with spefic classes

--- ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
+++ ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
@@ -1,7 +1,10 @@
 package no.ntnu.datakomm.chat;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
 
