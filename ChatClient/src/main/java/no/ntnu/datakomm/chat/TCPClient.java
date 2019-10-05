21fba4a88c75cbbebe96d8ed40c24b7b92306e93
nilsjha <nils@gaupne.net>
2019-10-06 01:23:59 +0200

    Implemented method for establishing connection

--- ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
+++ ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
@@ -23,10 +23,17 @@ public class TCPClient {
      * @return True on success, false otherwise
      */
     public boolean connect(String host, int port) {
-        // TODO Step 1: implement this method
-        // Hint: Remember to process all exceptions and return false on error
-        // Hint: Remember to set up all the necessary input/output stream variables
-        return false;
+        try {
+            connection = new Socket(host, port);
+            toServer = new PrintWriter(connection.getOutputStream(), true);
+            fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            System.out.println("[CLIENT]: Established " + connection.getInetAddress()
+             + ":" + connection.getPort() + ", srcPort:" + connection.getLocalPort());
+            return true;
+        } catch (IOException e) {
+            System.out.println("[CLIENT]: Socket error:" + e.getMessage());
+            return false;
+        }
     }
 
     /**
